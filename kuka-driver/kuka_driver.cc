2494f86a51d07958667b76ec81bb4a181aa6fe0b
Sam Creasey <sam.creasey@tri.global>
2016-06-28 16:07:54 -0400

    Convert the kuka driver into a minimal driver which isn't aware of trajectories.

--- kuka-driver/kuka_driver.cc
+++ kuka-driver/kuka_driver.cc
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <cmath>
 #include <cstring>
 
@@ -12,7 +13,6 @@
 
 #include "lcmtypes/drake/lcmt_iiwa_command.hpp"
 #include "lcmtypes/drake/lcmt_iiwa_status.hpp"
-#include "lcmtypes/drake/lcmt_iiwa_trajectory.hpp"
 
 namespace drake {
 namespace examples {
@@ -20,7 +20,7 @@ namespace kuka_iiwa_arm {
 namespace {
 
 const char* lcm_status_channel = "IIWA_STATUS";
-const char* lcm_plan_channel = "IIWA_PLAN";
+const char* lcm_command_channel = "IIWA_COMMAND";
 
 double ToRadians(double degrees) {
   return degrees * M_PI / 180.;
@@ -28,10 +28,10 @@ double ToRadians(double degrees) {
 
 class KukaLCMClient : public KUKA::FRI::LBRClient {
  public:
-  KukaLCMClient()
-      : plan_start_time_(0),
-        cur_plan_step_(-1) {
-    lcm_status_.timestamp = 0;
+  KukaLCMClient() {
+    // Use -1 as a sentinal to indicate that no status has been
+    // sent (or received from the robot).
+    lcm_status_.timestamp = -1;
     lcm_status_.num_joints = num_joints_;
     lcm_status_.joint_position_measured.resize(num_joints_);
     lcm_status_.joint_position_commanded.resize(num_joints_);
@@ -39,6 +39,10 @@ class KukaLCMClient : public KUKA::FRI::LBRClient {
     lcm_status_.joint_torque_measured.resize(num_joints_);
     lcm_status_.joint_torque_commanded.resize(num_joints_);
     lcm_status_.joint_torque_external.resize(num_joints_);
+
+    // Use -1 as a sentinal to indicate that no command has been
+    // received.
+    lcm_command_.timestamp = -1;
 
     // Joint limits derived from visual inspection of KUKA controller
     // output display.  Values in +/- degrees from center.
@@ -50,7 +54,8 @@ class KukaLCMClient : public KUKA::FRI::LBRClient {
     joint_limits_.push_back(ToRadians(120));
     joint_limits_.push_back(ToRadians(175));
 
-    lcm_.subscribe(lcm_plan_channel, &KukaLCMClient::HandlePlanMessage, this);
+    lcm_.subscribe(lcm_command_channel,
+                   &KukaLCMClient::HandleCommandMessage, this);
   }
 
   ~KukaLCMClient() {}
@@ -74,10 +79,24 @@ class KukaLCMClient : public KUKA::FRI::LBRClient {
   }
   virtual void command() {
     KUKA::FRI::LBRClient::command();
-    //    double pos[num_joints_] = { 0., 0., 0., 0., 0., 0., 0.};
-    //robotCommand().setJointPosition(pos);
     PublishStateUpdate();
-    AdvancePlan();
+
+    if (lcm_status_.timestamp == -1) {
+      return;
+    }
+
+    double pos[num_joints_] = { 0., 0., 0., 0., 0., 0., 0.};
+    if (lcm_command_.timestamp == -1) {
+      // No command received, just command the current position.
+      memcpy(pos, lcm_status_.joint_position_measured.data(),
+             num_joints_ * sizeof(double));
+    } else {
+      assert(lcm_command_.num_joints == num_joints_);
+      memcpy(pos, lcm_command_.joint_position.data(),
+             num_joints_ * sizeof(double));
+    }
+    ApplyJointLimits(pos);
+    robotCommand().setJointPosition(pos);
   }
 
  private:
@@ -87,15 +106,12 @@ class KukaLCMClient : public KUKA::FRI::LBRClient {
         pos[i] = std::max(std::min(pos[i], (joint_limits_[i] - joint_tol)),
                           ((-joint_limits_[i]) + joint_tol));
       }
-
   }
 
-  void HandlePlanMessage(const lcm::ReceiveBuffer* rbuf, const std::string& chan,
-                         const lcmt_iiwa_trajectory* plan) {
-    std::cerr << "Got plan, " << plan->num_states << " states" << std::endl;
-    plan_ = *plan;
-    cur_plan_step_ = 0;
-    plan_start_time_ = lcm_status_.timestamp;
+  void HandleCommandMessage(const lcm::ReceiveBuffer* rbuf,
+                            const std::string& chan,
+                            const lcmt_iiwa_command* command) {
+    lcm_command_ = *command;
   }
 
   void PublishStateUpdate() {
@@ -126,104 +142,12 @@ class KukaLCMClient : public KUKA::FRI::LBRClient {
     lcm_.handleTimeout(0);
   }
 
-  void AdvancePlan() {
-    if (lcm_status_.timestamp == 0) { return; }
-
-    double pos[num_joints_] = { 0., 0., 0., 0., 0., 0., 0.};
-    if (cur_plan_step_ < 0) {
-        // Startup, just command the current position.
-      memcpy(pos, lcm_status_.joint_position_measured.data(),
-             num_joints_ * sizeof(double));
-    } else {
-      std::cerr << "plan step " << cur_plan_step_ << std::endl;
-      const int64_t next_step_time =
-          plan_start_time_ + plan_.plan[cur_plan_step_].timestamp;
-      double time_to_next_step = next_step_time - lcm_status_.timestamp;
-            std::cerr << "next_step_time " << next_step_time
-                << " time_to_next_step " << time_to_next_step
-                << std::endl;
-      if (time_to_next_step <= 0.) {
-        std::cerr << "didn't reach next step in time!" << std::endl;
-        // TODO(sam.creasey) 0.1s is totally arbitrary.
-        time_to_next_step = 100;
-      }
-
-      const double step_size = 5.;
-      const double num_steps = time_to_next_step / step_size;
-      std::cerr << "remaining steps: " << num_steps << std::endl;
-      std::cerr << "commanding " << plan_.plan[cur_plan_step_].num_joints
-                << " joints at step " << cur_plan_step_
-                << std::endl;
-      double target_pos[num_joints_];
-      memcpy(target_pos, plan_.plan[cur_plan_step_].joint_position.data(),
-             plan_.plan[cur_plan_step_].num_joints * sizeof(double));
-
-      // TODO(sam.creasey) this is totally arbitrary
-      //const double max_joint_rate = 1.;  // rad/s
-      const double max_delta = std::min(0.1, 0.0005 * num_steps);  // rad
-      const double target_tol = 1e-2;  // rad
-      bool at_target = true;
-
-
-      std::cerr << "desired rate (command): ";
-      for (int i = 0; i < num_joints_; i++) {
-        std::cerr << target_pos[i];
-        double joint_delta = target_pos[i] - lcm_status_.joint_position_measured[i];
-        joint_delta = std::max(-max_delta,
-                               std::min(max_delta, joint_delta));
-#if 0
-        double joint_step = joint_delta / num_steps;
-        const double max_joint_step = (max_joint_rate * 1e3) / step_size;
-        joint_step = std::min(max_joint_step,
-                              std::max(-max_joint_step, joint_step));
-        std::cerr << " " << joint_step;
-#endif
-#if 0
-        double joint_rate = joint_delta / time_to_next_step;
-        if (std::fabs(joint_rate) > max_joint_rate) {
-          std::cerr << "capping rate of joint " << i << " commanded "
-                    << joint_rate << std::endl;
-          pos[i] = std::min(
-              std::max(pos[i], -(max_joint_rate * time_to_next_step)),
-              (max_joint_rate * time_to_next_step));
-        }
-#endif
-        pos[i] = lcm_status_.joint_position_measured[i] + joint_delta;
-        std::cerr << "(" << pos[i] << ") ";
-
-        if (std::fabs(joint_delta) > target_tol) {
-          at_target = false;
-        }
-      }
-      std::cerr << std::endl;
-
-      if (at_target) {
-        std::cerr << "Reached target for step " << cur_plan_step_
-                  << std::endl;
-        if (time_to_next_step < 200) {
-          cur_plan_step_++;
-        }
-        if (cur_plan_step_ >= plan_.num_states) {
-          cur_plan_step_ = plan_.num_states - 1;
-        }
-      }
-    }
-
-    // for (int i = 0; i < num_joints_; i++) {
-    //   pos[i] = 0;
-    // }
-    ApplyJointLimits(pos);
-    robotCommand().setJointPosition(pos);
-  }
-
   static const int num_joints_ = 7;
 
   lcm::LCM lcm_;
   lcmt_iiwa_status lcm_status_;
+  lcmt_iiwa_command lcm_command_;
   std::vector<double> joint_limits_;
-  int64_t plan_start_time_;
-  int cur_plan_step_;
-  lcmt_iiwa_trajectory plan_;
 };
 
 int do_main(int argc, const char* argv[]) {
