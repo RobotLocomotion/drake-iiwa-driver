1eee2a17894250ef11df182cfeb7a1183008c041
Sam Creasey <sam.creasey@tri.global>
2016-06-28 15:47:39 -0400

    Add java driver and update README.md on how to use it

--- README.md
+++ README.md
@@ -8,4 +8,42 @@ protocol.
 
 TODO(sam.creasey) Add a section explaining the FRI C++ portions, the externals, and building the resulting driver
 
-TODO(sam.creasey) Add a section explaining Sunrise Workbench and the Java application.
+
+## Sunrise Workbench
+
+Provisioning the IIWA arm must be done from Sunrise Workbench.
+
+TODO(sam.creasey) Can I just zip up a project/workspace?
+
+ * File -> New -> SunriseProject
+  * IP address of the controller can be left at 172.31.1.147
+  * Create new project
+  * Project Name: DrakeFRIDriver
+  * Topology template: LBR iiwa 14 R820
+  * Media Flange: Medien-Flansch Touch pneumatisch
+  * Create Application
+  * Source folder: whatever
+  * Package: drake_fri
+  * Name: DrakeFRIDriver
+
+ * In "Package Explorer", select StationSetup.cat
+  * Software (leave anything checked which already is, I think)
+   * Direct Servo Motion Extension (might not be needed?)
+   * Fast Robot Interface
+   * Smart Servo Motion Extension
+  * Save (Ctrl-S)
+
+ * In "Package Explorer", select SafetyConfiguration.sconf
+  * Customer PSM
+   * Uncheck row 1 "External EMERGENCY STOP"
+   * Uncheck row 2 "Operator Protection",
+   * Uncheck row 3 "Safety Stop"
+
+ * Copy in DrakeFRIDriver.java over the old one (make sure Sunrise sees the update) in DrakeFRIDriver/src/drake_fri/
+
+ * In "Package Explorer", select StationSetup.cat
+  * Installation
+    * Push "Install" (this will not actually install the application, but it will wipe the existing configuration of the KUKA cabinet and replace it with yours).  It will take a few minutes, and eventually will reboot the controller.
+
+ * Press the "sync" button.  It's on the toolbar at the top, 5th from the right.  It looks a bit like a square with a couple of arrows over it (though it doesn't look much like this).  This will install the application.
+  * Execute
---
+++ kuka-driver/DrakeFRIDriver.java
@@ -0,0 +1,107 @@
+package drake_fri;
+
+// This file is loosely based on the KUKA sample code for the FRI interface.
+
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.ptp;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.kuka.connectivity.fri.FRIConfiguration;
+import com.kuka.connectivity.fri.FRIJointOverlay;
+import com.kuka.connectivity.fri.FRISession;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import com.kuka.roboticsAPI.controllerModel.Controller;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.executionModel.CommandInvalidException;
+import com.kuka.roboticsAPI.motionModel.PositionHold;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.JointImpedanceControlMode;
+
+/**
+ * Creates a FRI Session.
+ */
+public class DrakeFRIDriver extends RoboticsAPIApplication
+{
+    private Controller _lbrController;
+    private LBR _lbr;
+    private String _clientName;
+
+    @Override
+    public void initialize()
+    {
+        _lbrController = (Controller) getContext().getControllers().toArray()[0];
+        _lbr = (LBR) _lbrController.getDevices().toArray()[0];
+        // **********************************************************************
+        // *** change next line to the FRIClient's IP address                 ***
+        // **********************************************************************
+        _clientName = "192.170.10.200";
+    }
+
+    private void doFRISession(FRIConfiguration friConfiguration) {
+        getLogger().info("Creating FRI connection to " + friConfiguration.getHostName());
+        getLogger().info("SendPeriod: " + friConfiguration.getSendPeriodMilliSec() + "ms |"
+                + " ReceiveMultiplier: " + friConfiguration.getReceiveMultiplier());
+
+        FRISession friSession = new FRISession(friConfiguration);
+        // wait until FRI session is ready to switch to command mode
+        try
+        {
+            friSession.await(3600, TimeUnit.SECONDS);
+        }
+        catch (final TimeoutException e)
+        {
+            getLogger().error(e.getLocalizedMessage());
+            friSession.close();
+            return;
+        }
+
+        getLogger().info("FRI connection established.");
+        FRIJointOverlay jointOverlay = new FRIJointOverlay(friSession);
+
+        // TODO(sam.creasey) It's probably not necessary to recenter
+        // every time we reconnect.
+        _lbr.move(ptp(0., 0., 0., 0., 0., 0., 0.));
+
+        // TODO(sam.creasey) Neither the selection of
+        // JointImpedenceControlMode, nor the particular impedence
+        // values, nor the damping values, have actually been
+        // calibrated (or really thought out at all).
+        JointImpedanceControlMode ctrMode =
+            new JointImpedanceControlMode(500, 500, 500, 500, 500, 500, 500);
+        ctrMode.setDamping(.7,.7,.7,.7,.7,.7,.7);
+        PositionHold posHold = new PositionHold(ctrMode, -1, TimeUnit.SECONDS);
+
+        try {
+          _lbr.move(posHold.addMotionOverlay(jointOverlay));
+        } catch (final CommandInvalidException e) {
+          getLogger().error(e.getLocalizedMessage());
+        }
+
+        // done
+        friSession.close();
+    }
+
+    @Override
+    public void run()
+    {
+        // configure and start FRI session
+        FRIConfiguration friConfiguration = FRIConfiguration.createRemoteConfiguration(_lbr, _clientName);
+        friConfiguration.setSendPeriodMilliSec(5);
+        while (true) {
+          doFRISession(friConfiguration);
+        }
+    }
+
+    /**
+     * main.
+     *
+     * @param args
+     *            args
+     */
+    public static void main(final String[] args)
+    {
+        final DrakeFRIDriver app = new DrakeFRIDriver();
+        app.runApplication();
+    }
+
+}
