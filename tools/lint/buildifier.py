071225165e186058454c7f3ad3c803974a0691ca
Jeremy Nimmer <jeremy.nimmer@tri.global>
2025-09-30 11:39:19 -0700

    Port to bzlmod (#52)
    Link the driver statically with lcm, glib, and pcre. This is simpler
    to implement in the BUILD logic, and allows the binary to run without
    worrying about dynamic library paths.

--- tools/lint/buildifier.py
+++
@@ -1,4 +0,0 @@
-from drake.tools.lint.buildifier import main
-
-if __name__ == "__main__":
-    main(workspace_name="drake_iiwa_driver")
--- tools/lint/clang_format_includes.py
+++
@@ -1,4 +0,0 @@
-from drake.tools.lint.clang_format_includes import main
-
-if __name__ == "__main__":
-    main(workspace_name="drake_iiwa_driver")
